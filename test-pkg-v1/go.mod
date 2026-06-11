e42a9d431439d1b0c68b02e9b64136f050c5d172
Grinish Nepal <grinish.nepal@aexp.com>
2026-06-11 16:28:22 -0700

    chore: use v1 package

--- test-pkg-v1/go.mod
+++ test-pkg-v1/go.mod
@@ -1,3 +1,5 @@
 module github.com/grinish21/test-mono-go/test-pkg-v1
 
 go 1.26.3
+
+require github.com/grinish21/test-mono-go/test-pkg-v1-v2 v1.0.0 // indirect
--- test-pkg-v1/main.go
+++ test-pkg-v1/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/grinish21/test-mono-go/test-pkg-v1-v2/who"
 )
 
 func main() {
 	fmt.Println("I am pkg-v1 only")
-
+	who.WhoAmI()
 }
 
 func WhoAmI() string {
